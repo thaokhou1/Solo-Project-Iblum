93e780111694f1d849a5a3fdb1114ba79ff947d1
Khou Thao <khou@Khous-MacBook-Air.local>
2019-10-27 21:02:01 -0500

    Deleted unnecessary code

--- src/components/PictureItem/PictureItem.js
+++ src/components/PictureItem/PictureItem.js
@@ -13,48 +13,7 @@ class PictureItem extends Component {
     }
     render() {
         return (         
-            // <li >
-
-            //     <div className="container" >
-            //         <img  src={this.props.image.image} width="220" height="220"  hspace="20"/>
-            //         <div class="overlay">
-            //         <h3> {moment(this.props.image.date).format('MM/DD/YYYY')}</h3>
-            //         <h3>{this.props.image.location}</h3>
-            //         <button onClick={this.playAudio}>
-            //             <span>Play Audio</span>
-            //         </button>
-            //         <br></br>
-            //         <audio className="audio-element">
-            //             <source src={this.props.image.audio}></source>
-            //         </audio>
-            //         <button onClick={this.removepicture}>Delete</button>
-            //         </div>
-            //     </div>
-            // </li>
-            /*--------second layout------ */
-            //      <div className="flip-box">
-            //         <div className="flip-box-inner">
-            // <li >
-
-            //     <div className="flip-box-front">
-            //     <img className="cardImg" src={this.props.image.image} />
-            //     </div>
-            //     <div className="flip-box-back">
-            //     <h3> {moment(this.props.image.date).format('MM/DD/YYYY')}</h3>
-            //     <h3>{this.props.image.location}</h3>
-            //     <button onClick={this.playAudio}>
-            //         <span>Play Audio</span>
-            //     </button>
-            //     <br></br>
-            //     <audio className="audio-element">
-            //         <source src={this.props.image.audio}></source>
-            //     </audio>
-            //     <button onClick={this.removepicture}>Delete</button>
-            //     </div>
-            // </li>
-            // </div>
-            //     </div>
-            /* ------- First lay out-------*/
+   
             <div>
                 <div className="flip-box">
                     <div className="flip-box-inner">
